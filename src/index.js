c043159865a08bd75b481d9bcbe42346d15265d8
gvergnaud <gabriel.vergnaud@gmail.com>
2019-02-15 23:01:15 +0100

    Improve perfs

--- src/index.js
+++ src/index.js
@@ -1,71 +1,67 @@
-import { Observable, of, combineLatest } from 'rxjs'
+import { Observable } from 'rxjs';
 
 export const mapValues = (f, obj) =>
-  Object.keys(obj).reduce((acc, k) => ({ ...acc, [k]: f(obj[k], k) }), {})
-
-const map = f => stream =>
-  new Observable(observer =>
-    stream.subscribe({
-      complete: () => observer.complete(),
-      error: e => observer.error(e),
-      next: x => observer.next(f(x))
-    })
-  )
+  Object.keys(obj).reduce((acc, k) => ({ ...acc, [k]: f(obj[k], k) }), {});
 
 const switchMap = switchMapper => stream => {
-  let subscription
+  let subscription;
 
   return new Observable(observer => {
-    let isOuterStreamComplete = false
-    let isInnerStreamComplete = false
+    let isOuterStreamComplete = false;
+    let isInnerStreamComplete = false;
 
     const sub = stream.subscribe({
       next: x => {
-        if (subscription) subscription.unsubscribe()
+        if (subscription) subscription.unsubscribe();
         subscription = switchMapper(x).subscribe({
           error: e => observer.error(e),
           next: x => observer.next(x),
           complete: () => {
-            isInnerStreamComplete = true
-            if (isOuterStreamComplete) observer.complete()
+            isInnerStreamComplete = true;
+            if (isOuterStreamComplete) observer.complete();
           }
-        })
+        });
       },
       error: e => observer.error(e),
       complete: () => {
-        isOuterStreamComplete = true
-        if (isInnerStreamComplete) observer.complete()
+        isOuterStreamComplete = true;
+        if (isInnerStreamComplete) observer.complete();
       }
-    })
+    });
 
     return {
       unsubscribe: () => {
-        if (subscription) subscription.unsubscribe()
-        sub.unsubscribe()
+        if (subscription) subscription.unsubscribe();
+        sub.unsubscribe();
       }
-    }
-  })
-}
+    };
+  });
+};
 
-const defaultSecondPerFrame = 0.016
+const defaultSecondPerFrame = 0.016;
 
 const rafThrottle = f => {
-  let shouldExecute = true
-  let args = []
+  let isFirst = true;
+  let shouldExecute = true;
+  let args = [];
   return (..._args) => {
-    args = _args
-    if (!shouldExecute) return
-    shouldExecute = false
-
-    window.requestAnimationFrame(() => {
-      shouldExecute = true
-      f(...args)
-    })
-  }
-}
+    if (isFirst) {
+      isFirst = false;
+      f(...args);
+    } else {
+      args = _args;
+      if (!shouldExecute) return;
+      shouldExecute = false;
+      window.requestAnimationFrame(() => {
+        shouldExecute = true;
+        f(...args);
+      });
+    }
+  };
+};
 
 // stepper :: Number -> Number -> Number -> Number? -> Number? -> Number? -> [Number, Number]
-let reusedTuple = [0, 0]
+let reusedTuple = [0, 0];
 function stepper(
   value,
   velocity,
@@ -79,45 +75,45 @@ function stepper(
 
   // for animations, destValue is really spring length (spring at rest). initial
   // position is considered as the stretched/compressed position of a spring
-  const Fspring = -stiffness * (value - destValue)
+  const Fspring = -stiffness * (value - destValue);
 
   // Damping, in kg / s
-  const Fdamper = -damping * velocity
+  const Fdamper = -damping * velocity;
 
   // usually we put mass here, but for animation purposes, specifying mass is a
   // bit redundant. you could simply adjust k and b accordingly
   // let a = (Fspring + Fdamper) / mass
-  const a = Fspring + Fdamper
+  const a = Fspring + Fdamper;
 
-  const newVelocity = velocity + a * secondPerFrame
-  const newValue = value + newVelocity * secondPerFrame
+  const newVelocity = velocity + a * secondPerFrame;
+  const newValue = value + newVelocity * secondPerFrame;
 
   if (
     Math.abs(newVelocity) < precision &&
     Math.abs(newValue - destValue) < precision
   ) {
-    reusedTuple[0] = destValue
-    reusedTuple[1] = 0
-    return reusedTuple
+    reusedTuple[0] = destValue;
+    reusedTuple[1] = 0;
+    return reusedTuple;
   }
 
-  reusedTuple[0] = newValue
-  reusedTuple[1] = newVelocity
-  return reusedTuple
+  reusedTuple[0] = newValue;
+  reusedTuple[1] = newVelocity;
+  return reusedTuple;
 }
 
 // createEasedStream :: (number, number) -> number -> Observable number
 const createEasedStream = (stiffness, damping) => {
-  let value
-  let velocity = 0
-  let destValue
+  let value;
+  let velocity = 0;
+  let destValue;
 
   return x => {
-    destValue = x
-    if (value === undefined) value = x
+    destValue = x;
+    if (value === undefined) value = x;
 
     return new Observable(observer => {
-      let isRunning = true
+      let isRunning = true;
 
       const run = rafThrottle(() => {
         [value, velocity] = stepper(
@@ -126,75 +122,130 @@ const createEasedStream = (stiffness, damping) => {
           destValue,
           stiffness,
           damping
-        )
+        );
 
-        observer.next(value)
+        observer.next(value);
         if (velocity !== 0 && isRunning) {
-          run()
+          run();
         }
-      })
+      });
 
-      run()
+      run();
 
       return {
         unsubscribe() {
-          isRunning = false
+          isRunning = false;
         }
-      }
-    })
-  }
-}
+      };
+    });
+  };
+};
 
-const combineLatestObject = obj =>
-  combineLatest(
-    ...Object.keys(obj).map(k => obj[k].pipe(map(v => [k, v]))),
-    (...entries) => entries.reduce((acc, [k, v]) => ({ ...acc, [k]: v }), {})
-  )
+// createEasedStreamArray :: [[number, number]] -> [number] -> Observable [number]
+const createEasedStreamArray = configs => {
+  let values;
+  let velocities = configs.map(() => 0);
+  let destValues;
 
-const combineLatestArray = xs$ =>
-  combineLatest(
-    ...xs$,
-    (...xs) => xs
-  )
+  return xs => {
+    destValues = xs;
+    if (values === undefined) values = xs;
 
-const createEase = (stiffness, damping) => {
-  if (Array.isArray(stiffness)) {
-    const eases = stiffness.map(
-      ([stiffness, damping]) => createEasedStream(stiffness, damping)
-    )
+    return new Observable(observer => {
+      let isRunning = true;
 
-    return values =>
-      combineLatestArray(
-        values.map((x, i) => (eases[i] ? eases[i](x) : of(x)))
-      )
-  }
+      const run = rafThrottle(() => {
+        for (let i = 0; i < configs.length; i++) {
+          const [stiffness, damping] = configs[i];
+          [values[i], velocities[i]] = stepper(
+            values[i],
+            velocities[i],
+            destValues[i],
+            stiffness,
+            damping
+          );
+        }
 
-  if (typeof stiffness === 'object') {
-    const eases = mapValues(
-      ([stiffness, damping]) => createEasedStream(stiffness, damping),
-      stiffness
-    )
+        observer.next(values);
+        if (velocities.some(velocity => velocity !== 0) && isRunning) {
+          run();
+        }
+      });
 
-    return values =>
-      combineLatestObject(
-        mapValues((x, k) => (eases[k] ? eases[k](x) : of(x)), values)
-      )
-  }
+      run();
 
-  return createEasedStream(stiffness, damping)
-}
+      return {
+        unsubscribe() {
+          isRunning = false;
+        }
+      };
+    });
+  };
+};
 
-const cache = new Map()
+// createEasedStreamObject :: Map k [number, number] -> Map k number -> Observable (Map k number)
+const createEasedStreamObject = configs => {
+  let values;
+  let velocities = mapValues(() => 0, configs);
+  let destValues;
+
+  return obj => {
+    destValues = obj;
+    if (values === undefined) values = obj;
+
+    return new Observable(observer => {
+      let isRunning = true;
+
+      const run = rafThrottle(() => {
+        for (let k in configs) {
+          const [stiffness, damping] = configs[k];
+          [values[k], velocities[k]] = stepper(
+            values[k],
+            velocities[k],
+            destValues[k],
+            stiffness,
+            damping
+          );
+        }
+
+        observer.next(values);
+        if (
+          Object.values(velocities).some(velocity => velocity !== 0) &&
+          isRunning
+        ) {
+          run();
+        }
+      });
+
+      run();
+
+      return {
+        unsubscribe() {
+          isRunning = false;
+        }
+      };
+    });
+  };
+};
+
+const createEase = (stiffness, damping) =>
+  Array.isArray(stiffness)
+    ? createEasedStreamArray(stiffness)
+    : typeof stiffness === 'object'
+    ? createEasedStreamObject(stiffness)
+    : createEasedStream(stiffness, damping);
+
+const cache = new Map();
 
 const getEase = (stiffness, damping, id) => {
-  if (id === undefined && typeof stiffness === 'object') id = damping
+  if (id === undefined && typeof stiffness === 'object') id = damping;
 
-  if (id === undefined) return createEase(stiffness, damping)
+  if (id === undefined) return createEase(stiffness, damping);
 
-  if (!cache.has(id)) cache.set(id, createEase(stiffness, damping))
-  return cache.get(id)
-}
+  if (!cache.has(id)) cache.set(id, createEase(stiffness, damping));
+  return cache.get(id);
+};
 
-const easeOperator = (...args) => switchMap(getEase(...args))
+const easeOperator = (...args) => switchMap(getEase(...args));
 
-export default easeOperator
+export default easeOperator;
